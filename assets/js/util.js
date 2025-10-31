0a297496cf779c8e0f3229f22a732509863df50e
Kestin Goforth <kgoforth1503@gmail.com>
2025-10-31 13:42:45 -0400

    Add numeric and converted bindings

--- assets/js/util.js
+++ assets/js/util.js
@@ -12,6 +12,33 @@ ko.bindingHandlers.fileUpload = {
   },
 };
 
+ko.bindingHandlers.numeric = {
+    update: function(element, valueAccessor, allBindings) {
+        let value = ko.unwrap(valueAccessor());
+        let precision = ko.unwrap(allBindings.get('precision')) || 1; // Default to 1 decimal places
+        let formattedValue = parseFloat(value).toFixed(precision);
+        if(element.tagName.toLowerCase() == "input")
+          ko.applyBindingsToNode(element, { value: formattedValue });
+        else
+          ko.applyBindingsToNode(element, { text: formattedValue });
+    }
+};
+
+ko.bindingHandlers.converted = {
+    update: function(element, valueAccessor, allBindings) {
+        let value = ko.unwrap(valueAccessor());
+        let fromUnit = ko.unwrap(allBindings.get('from'));
+        let toUnit = ko.unwrap(allBindings.get('to'));
+        let convertedVal = _convert(value, fromUnit, toUnit)
+        let precision = ko.unwrap(allBindings.get('precision')) || 1; // Default to 1 decimal places
+        let formattedValue = parseFloat(convertedVal).toFixed(precision);
+        if(element.tagName.toLowerCase() == "input")
+          ko.applyBindingsToNode(element, { value: formattedValue });
+        else
+          ko.applyBindingsToNode(element, { text: formattedValue });
+    }
+};
+
 function _convert(v, from, to){
     return math.unit(parseFloat(v), from).toNumber(to);
 }
