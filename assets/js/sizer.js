03facfa7dbc56f121e6bdc0dbff3b04efc36863f
Kestin Goforth <kgoforth1503@gmail.com>
2025-11-11 20:55:44 -0500

    Update map sizer tool

--- assets/js/sizer.js
+++ assets/js/sizer.js
@@ -1,108 +1,147 @@
-var canvas = undefined;
-var mapImg = undefined;
+class ViewModel {
+  constructor() {
+    var self = this;
 
-var mouseMode = undefined;
+    // self.turboList = TURBOS.filter(t => t.map_range.length == 0);
+    self.turboList = TURBOS.filter(t => t.flow_img && t.flow_range.length == 0);
 
-$("#selectOrigin").on("click", () => {
-    mouseMode = "origin";
-    $("#map").css('cursor', 'crosshair');
-})
+    self.canvas = $("#map")[0];
+    self.mapImg = new Image;
 
-$("#selectMax").on("click", () => {
-    mouseMode = "max";
-    $("#map").css('cursor', 'crosshair');
-})
+    self.turbo = ko.observable();
+    self.turbo.subscribe((t) => {
+      if(self.mapType() === "Compressor Map")
+        self.imgUrl(t.map_img);
+      else
+        self.imgUrl(t.flow_img);
+    });
+    self.imgUrl = ko.observable("/Turbo-Mapper/assets/img/maps/td04h-16t.gif");
+    self.mapType = ko.observable("Compressor Map")
+    self.mouseMode = ko.observable(undefined);
 
-$("#map").on("click", (e) => {
-    console.log(e);
-    let rect = e.target.getBoundingClientRect();
-    let scale = rect.width / mapImg.width;
-    let x = (e.clientX - rect.left) / scale;
-    let y = (rect.bottom - e.clientY) / scale;
-    console.log(x, y, scale, mapImg.width);
-    if(mouseMode){
-        if(mouseMode === 'origin') {
-            $("#bbox_xmin").val(x)
-            $("#bbox_ymin").val(y)
-        }
-        else if(mouseMode === 'max') {
-            $("#bbox_xmax").val(x)
-            $("#bbox_ymax").val(y)
-        }
-        mouseMode = undefined;
-        $("#map").css('cursor', 'inherit');
+    self.minPosX = ko.observable(0);
+    self.maxPosX = ko.observable(1);
+    self.minPosY = ko.observable(0);
+    self.maxPosY = ko.observable(1);
 
-        redraw();
+    self.minValX = ko.observable(0);
+    self.maxValX = ko.observable(100);
+    self.minValY = ko.observable(1);
+    self.maxValY = ko.observable(4);
+
+    self.xScale = ko.computed(() => (self.maxValX() - self.minValX()) / (self.maxPosX() - self.minPosX()));
+    self.yScale = ko.computed(() => (self.maxValY() - self.minValY()) / (self.maxPosY() - self.minPosY()));
+
+    self.imgMinX = ko.computed(() => self.minValX() - self.xScale() * self.minPosX());
+    self.imgMaxX = ko.computed(() => self.xScale() * self.mapImg.width + self.imgMinX());
+    self.imgMinY = ko.computed(() => self.minValY() - self.yScale() * self.minPosY());
+    self.imgMaxY = ko.computed(() => self.yScale() * self.mapImg.height + self.imgMinY());
+
+    self.mapRange = ko.computed(() => [self.imgMinX(), self.imgMaxX(), self.imgMinY(), self.imgMaxY()]);
+
+
+    self.loadNewImg = function() {
+      var newUrl = window.prompt("Enter Map Image URL:");
+      self.imgUrl(newUrl);
     }
-})
 
-function loadImg(url){
-    mapImg.onload = () => {
-        // canvas.width = mapImg.width;
-        canvas.height = canvas.width * (mapImg.height / mapImg.width);
-        $(canvas).css('background-image', 'url("' + url + '")');
+    self.redraw = function () {
+      let canvas = self.canvas;
+      let mapImg = self.mapImg;
 
-        $("#bbox_xmax").val(mapImg.width);
-        $("#bbox_xmax").attr('max', mapImg.width);
-        $("#bbox_ymax").val(mapImg.height);
-        $("#bbox_ymax").attr('max', mapImg.height);
+      let ctx = canvas.getContext("2d");
+      let scale = canvas.width / mapImg.width;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.rect(
+          self.minPosX() * scale,
+          canvas.height - self.maxPosY() * scale,
+          (self.maxPosX() - self.minPosX()) * scale,
+          (self.maxPosY() - self.minPosY()) * scale
+      );
+      ctx.stroke();
+    }
 
-        redraw();
+    self.handleCanvasClick = function (_, e) {
+      if(self.mouseMode() != "none" || e.shiftKey || e.ctrlKey){
+        let rect = e.target.getBoundingClientRect();
+        let scale = rect.width / self.mapImg.width;
+        let x = (e.clientX - rect.left) / scale;
+        let y = (rect.bottom - e.clientY) / scale;
+          if(self.mouseMode() === "origin" || e.shiftKey) {
+              self.minPosX(x);
+              self.minPosY(y);
+          }
+          else if(self.mouseMode() === "max" || e.ctrlKey) {
+              self.maxPosX(x);
+              self.maxPosY(y);
+          }
+          self.mouseMode("none");
+          self.redraw();
+      }
     };
-    mapImg.src = url;
+
+    self.clearPos = function() {
+      self.minPosX(0);
+      self.minPosY(0);
+      self.maxPosX(self.mapImg.width);
+      self.maxPosY(self.mapImg.height);
+      self.redraw();
+    };
+
+    self.imgUrl.subscribe((url) => {
+      self.updateUrlParams();
+      self.mapImg.onload = () => {
+          self.canvas.height = self.canvas.width * (self.mapImg.height / self.mapImg.width);
+          // $(self.canvas).css('background-image', 'url("' + url + '")');
+          self.redraw();
+      };
+      self.mapImg.src = url;
+    });
+
+    [self.minPosX, self.maxPosX, self.minPosY, self.maxPosY].forEach(e => e.subscribe(self.redraw));
+
+    // URL Parameters
+    self.updateUrlParams = function () {
+      let params = new URLSearchParams();
+      params.set("img", self.imgUrl());
+      params.set("px0", self.minPosX());
+      params.set("px1", self.maxPosX());
+      params.set("py0", self.minPosY());
+      params.set("py1", self.maxPosY());
+      params.set("vx0", self.minValX());
+      params.set("vx1", self.maxValX());
+      params.set("vy0", self.minValY());
+      params.set("vy1", self.maxValY());
+      history.replaceState(null, "", "?" + params.toString());
+    };
+    [
+      self.imgUrl,
+      self.minPosX,
+      self.maxPosX,
+      self.minPosY,
+      self.maxPosY,
+      self.minValX,
+      self.maxValX,
+      self.minValY,
+      self.maxValY,
+    ].forEach(e => e.subscribe(self.updateUrlParams));
+
+    self.loadFromUrlParams = function () {
+      let params = new URLSearchParams(window.location.search);
+      if (params.has("img")) self.imgUrl(params.get("img"));
+      if (params.has("px0")) self.minPosX(parseFloat(params.get("px0")));
+      if (params.has("px1")) self.maxPosX(parseFloat(params.get("px1")));
+      if (params.has("py0")) self.minPosY(parseFloat(params.get("py0")));
+      if (params.has("py1")) self.maxPosY(parseFloat(params.get("py1")));
+      if (params.has("vx0")) self.minValX(parseFloat(params.get("vx0")));
+      if (params.has("vx1")) self.maxValX(parseFloat(params.get("vx1")));
+      if (params.has("vy0")) self.minValY(parseFloat(params.get("vy0")));
+      if (params.has("vy1")) self.maxValY(parseFloat(params.get("vy1")));
+    };
+    self.loadFromUrlParams();
+  }
 }
 
-function redraw(){
-    let ctx = canvas.getContext("2d");
-    let scale = canvas.width / mapImg.width;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.rect(
-        $("#bbox_xmin").val() * scale,
-        canvas.height - $("#bbox_ymax").val() * scale,
-        ($("#bbox_xmax").val() - $("#bbox_xmin").val()) * scale,
-        ($("#bbox_ymax").val() - $("#bbox_ymin").val()) * scale
-    );
-    ctx.stroke();
-
-    let x_scale = ($("#val_xmax").val() - $("#val_xmin").val()) / ($("#bbox_xmax").val() - $("#bbox_xmin").val())
-    let y_scale = ($("#val_ymax").val() - $("#val_ymin").val()) / ($("#bbox_ymax").val() - $("#bbox_ymin").val())
-
-    let x_min = $("#val_xmin").val() - x_scale * $("#bbox_xmin").val();
-    let x_max = x_scale * mapImg.width + x_min;
-
-    let y_min = $("#val_ymin").val() - y_scale * $("#bbox_ymin").val();
-    let y_max = y_scale * mapImg.height + y_min;
-
-    let output = `{
-  name: "${$("#name").val()}",
-  description: "",
-  map_img: "${mapImg.src.replace('http://localhost:4000/', '/')}",
-  map_range: [${x_min}, ${x_max}, ${y_min}, ${y_max}],
-  map_unit: "${$("#x_units").val()}",
-  flow_img: "",
-  flow_range: [],
-  flow_unit: "",
-  product_url: ""
-},`;
-    $("#output").text(output);
-}
-
-$(window).on('load', (e) => {
-    canvas = $("#map")[0];
-    mapImg = new Image;
-    loadImg("maps/td04h-16t.gif");
-});
-
-$("#load_map_img").on("click", (e) => {
-    var newUrl = window.prompt("Enter Map Image URL:");
-    loadImg(newUrl);
-});
-
-$("#bbox_xmin, #bbox_xmax, #bbox_ymin, #bbox_ymax").on('change', (e) => {
-    redraw();
-});
-$("#val_xmin, #val_xmax, #val_ymin, #val_ymax").on('change', (e) => {
-    redraw();
-});
+ko.applyBindings(new ViewModel());
